79189f9de77c17994e9d0ff0e4a89829398b3a7f
Jackmeson1 <130693642+Jackmeson1@users.noreply.github.com>
2025-06-06 15:54:19 -0700

    Remove backup from 28Apr2022

--- pysys_backup/28Apr2022/PySystem/sysrunner/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-pysys_root: str = Path(os.path.dirname(os.path.abspath(__file__))).parent
-sys.path.append(pysys_root)
-
--- pysys_backup/28Apr2022/PySystem/sysrunner/system_init.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-pysys_root: str = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(pysys_root)
